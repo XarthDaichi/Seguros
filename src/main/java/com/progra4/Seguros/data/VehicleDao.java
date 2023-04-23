878f52e060357c736393b2b55d4c4cfd26131fb7
xGtx1304x <83380523+xGtx1304x@users.noreply.github.com>
2023-04-22 23:24:31 -0600

    Update VehicleDao.java

--- src/main/java/com/progra4/Seguros/data/VehicleDao.java
+++ src/main/java/com/progra4/Seguros/data/VehicleDao.java
@@ -4,10 +4,47 @@
  */
 package com.progra4.Seguros.data;
 
+import com.progra4.Seguros.logic.Vehicle;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author diego
  */
 public class VehicleDao {
+    RelDatabase db;
     
+    public VehicleDao(RelDatabase db) {
+        this.db = db;
+    }
+    
+    public Vehicle read(String id) throws Exception {
+        String sql = "select " +
+                "* " +
+                "from  Vehicle v " +
+                "where v.licensePlate=?";
+        PreparedStatement stm = db.prepareStatement(sql);
+        stm.setString(1, id);
+        ResultSet rs = db.executeQuery(stm);
+        if (rs.next()) {
+            return from(rs, "v");
+        } else {
+            throw new Exception("Vehiculo no Existe");
+        }
+    }
+    
+    public Vehicle from(ResultSet rs, String alias) {
+        try {
+            Vehicle e = new Vehicle();
+            e.setLicensePlate(rs.getString(alias + ".licensePlate"));
+            e.setBrand(rs.getString(alias + ".brand"));
+            e.setModel(rs.getString(alias + ".model"));
+            e.setYear(rs.getInt(alias + ".yearV"));            
+            return e;
+        } catch (SQLException ex) {
+            return null;
+        }
+    }
 }
