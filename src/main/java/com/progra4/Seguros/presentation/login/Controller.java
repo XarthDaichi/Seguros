90bf98ac34b03a4e41c09542382aa2cbede5cc44
xGtx1304x <83380523+xGtx1304x@users.noreply.github.com>
2023-04-23 00:41:38 -0600

    sí

--- src/main/java/com/progra4/Seguros/presentation/login/Controller.java
+++ src/main/java/com/progra4/Seguros/presentation/login/Controller.java
@@ -4,6 +4,7 @@
  */
 package com.progra4.Seguros.presentation.login;
 
+import com.progra4.Seguros.logic.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -11,13 +12,16 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author Dell
  */
 @WebServlet(name = "LoginController", urlPatterns = {"/presentation/login/show",
-    "/presentation/login/register", "/presentation/login/login"})
+    "/presentation/login/register", "/presentation/login/login","/presentation/login/logout"})
 public class Controller extends HttpServlet {
 
     /**
@@ -32,7 +36,7 @@ public class Controller extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        //request.setAttribute("model", new Model()); 
+        request.setAttribute("model", new Model()); 
         
         String viewUrl="";
         switch(request.getServletPath()){
@@ -44,6 +48,10 @@ public class Controller extends HttpServlet {
                 break;
             case "/presentation/login/login":
                 viewUrl=this.login(request);
+                break;
+            case "/presentation/login/logout":
+                viewUrl=this.logout(request);
+                break;
         }
         request.getRequestDispatcher(viewUrl).forward( request, response);
     }
@@ -74,7 +82,45 @@ public class Controller extends HttpServlet {
     }
     
     public String login(HttpServletRequest request){
-        
+        try{
+            this.updateModel(request);          
+            return this.loginAction(request);            
+        }
+        catch(Exception e){
+            return "/presentation/Error.jsp";
+        }
+    }
+    
+    public String loginAction(HttpServletRequest request) {
+        Model model= (Model) request.getAttribute("model");
+        Service  service = Service.instance();
+        HttpSession session = request.getSession(true);
+        try {
+            User real = service.userFind(model.getCurrent().getId(), model.getCurrent().getPassword());
+            model.getCurrent().setAdministrator(real.getAdministrator());
+            session.setAttribute("user", real);
+            String viewUrl="";
+            if (real.getAdministrator()) viewUrl="/presentation/cliente/cuentas/show";
+            else viewUrl="";
+            return viewUrl;
+        } catch (Exception ex) {
+            Map<String,String> errores = new HashMap<>();
+            request.setAttribute("errores", errores);
+            errores.put("cedulaFld","Usuario o clave incorrectos");
+            errores.put("claveFld","Usuario o clave incorrectos");
+            return "/presentation/login/View.jsp"; 
+        }
+    }
+    
+    public String logout(HttpServletRequest request){
+        return this.logoutAction(request);
+    }
+    
+    public String logoutAction(HttpServletRequest request){
+        HttpSession session = request.getSession(true);
+        session.removeAttribute("user");
+        session.invalidate();
+        return "/presentation/login/show";
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
