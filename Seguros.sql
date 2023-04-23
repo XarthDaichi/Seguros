f8244423cfb13f7d5a21120262a00b43df4ba21d
XarthDaichi <diegoqahh@gmail.com>
2023-04-23 17:52:14 -0600

    fixes to the databse

--- Seguros.sql
+++ Seguros.sql
@@ -36,7 +36,7 @@ create table Coverage(
     descrip varchar(60),
     cost double,
     percent double,
-    categoryId varchar(30),
+    categoryId int,
     constraint PKCoverage Primary Key(coverageId),
     constraint Foreign key (categoryId) references Category(categoryId)
 );
@@ -54,24 +54,24 @@ create table PolicyClass (
 );
 
 create table Applies (
-    policyId varchar(30),
-    coverageId varchar(30),
+    policyId int,
+    coverageId int,
     constraint PKApplies Primary Key (policyId, coverageId),
     constraint Foreign Key (policyId) references PolicyClass(policyId),
     constraint Foreign Key (coverageId) references Coverage(coverageId)
 );
 
-insert into Category (categoryId, categoryName, descrip) 
-values ('Cat001', 'Responsabilidad Civil', 'testing 1'), 
-('Cat002', 'Daño Directo', 'testing 2');
+insert into Category (categoryName, descrip) 
+values ('Responsabilidad Civil', 'testing 1'), 
+('Daño Directo', 'testing 2');
 
-insert into Coverage (coverageId, coverageName, descrip, cost, percent, categoryId) 
-values ('Cov001', 'Daño a Personas', 'testing cov 1', 2.00, 0.5, 'Cat001'), 
-('Cov002', 'Daño a Bienes', 'testing cov 2', 2.05, 0.4, 'Cat001'),
-('Cov003', 'Gastos Legales', 'testing cov 3', 2.00, 0.5, 'Cat001'),
-('Cov004', 'Daño Directo', 'testing cov 4', 1.00, 0.3, 'Cat002'),
-('Cov005', 'Daño al auto', 'testing cov 5', 2.00, 0.7, 'Cat002'),
-('Cov006', 'Robo', 'testing cov 6', 3.50, 0.8, 'Cat002');
+insert into Coverage (coverageName, descrip, cost, percent, categoryId) 
+values ('Daño a Personas', 'testing cov 1', 2.00, 0.5, 1), 
+('Daño a Bienes', 'testing cov 2', 2.05, 0.4, 1),
+('Gastos Legales', 'testing cov 3', 2.00, 0.5, 1),
+('Daño Directo', 'testing cov 4', 1.00, 0.3, 2),
+('Daño al auto', 'testing cov 5', 2.00, 0.7, 2),
+('Robo', 'testing cov 6', 3.50, 0.8, 2);
 
 insert into Users (userId, pass, nameU, cellphone, email, cardNumber, typeU) values ('111','111','Diego','11111111', 'diego@testing.com','111',0),
 ('222','222','Jorge','22222222','jorge@testing.com','222',1), 
@@ -79,5 +79,5 @@ insert into Users (userId, pass, nameU, cellphone, email, cardNumber, typeU) val
 
 insert into Vehicle (licensePlate, brand, model, yearV) values ('ABC123', 'Toyota', 'Tercel', 1996), ('LRD596', 'LandRover', 'RangeRover', 2023);
 
-insert into policyclass (policyId, userId, vehicleLicensePlate, term, initialDate, insuredValue) values('1', '111', 'ABC123', 'QUARTERLY', '2023-04-23', 3000.5),
-('2', '333', 'LRD596', 'BIANNUAL', '2023-04-21', 2000000.0);
+insert into policyclass (userId, vehicleLicensePlate, term, initialDate, insuredValue) values('111', 'ABC123', 'QUARTERLY', '2023-04-23', 3000.5),
+('333', 'LRD596', 'BIANNUAL', '2023-04-21', 2000000.0);
