beaa231360d0ab6b311ac611cdf349c67ba48153
LewMontes <101229716+LewMontes@users.noreply.github.com>
2023-05-28 15:01:32 -0600

    Created variables and functions (test pending)

--- SegurosFrontEnd/src/main/webapp/js/index.js
+++ SegurosFrontEnd/src/main/webapp/js/index.js
@@ -4,3 +4,17 @@
  */
 
 
+var backend = "http://localhost:8080/SegurosBackEnd/api";
+
+var globalstate = {user:null};
+
+function lodaded(){
+    app = new App();
+    document.querySelector('#root').replaceChildren(app.dom);
+}
+
+document.addEventListener("DOMContentLoaded", loaded);
+
+function errorMessage(code){
+    alert(`Error. Status: ${code}`);
+}
