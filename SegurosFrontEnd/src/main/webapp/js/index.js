3a4de35468ae692e93e3aa95ed05c3a8f996b384
LewMontes <101229716+LewMontes@users.noreply.github.com>
2023-06-01 10:42:19 -0600

    Fixed missing variable and deleted trash comments

--- SegurosFrontEnd/src/main/webapp/js/index.js
+++ SegurosFrontEnd/src/main/webapp/js/index.js
@@ -1,12 +1,8 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
- */
-
-
 var backend = "http://localhost:8080/SegurosBackEnd/api";
 
 var globalstate = {user:null};
+
+var app;
 
 function lodaded(){
     app = new App();
