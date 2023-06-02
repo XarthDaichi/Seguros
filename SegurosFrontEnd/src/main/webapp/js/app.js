e2a16c17cb82c7d5019d5e5a30d2f1754a6130b3
xGtx1304x <83380523+xGtx1304x@users.noreply.github.com>
2023-06-02 11:59:18 -0600

    Update app.js

--- SegurosFrontEnd/src/main/webapp/js/app.js
+++ SegurosFrontEnd/src/main/webapp/js/app.js
@@ -61,6 +61,7 @@ class App{
             <footer id="footer" class="bg-dark text-white mt-4 w-100 fixed-bottom">
                 <div class="container">
                     <div class="row">
+<<<<<<< Updated upstream
                       <div class="col-md-4">
                         <h4>Sobre nosotros</h4>
                         <p>
@@ -89,6 +90,36 @@ class App{
                 </div>
                 <div class="bottom">
                     <p>&copy; 2023 - Todos los derechos reservados</p>
+=======
+                        <div class="col-md-4">
+                            <h4>Sobre nosotros</h4>
+                            <p>
+                                Somos una empresa
+                                multinacional comprometida a asegurar de manera eficiente sus bienes.
+                            </p>
+                        </div>
+                        <div class="col-md-4">
+                            <h4>Contactenos</h4>
+                            <ul>
+                                <li><i class="fas fa-map-marker-alt"></i> Heredia, Costa Rica</li>
+                                <li><i class="fas fa-phone"></i> (506) 6304-0703</li>
+                                <li><i class="fas fa-envelope"></i> jorgedc1304@gmail.com</li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4">
+                            <h4>Redes Sociales</h4>
+                            <ul class="social">
+                              <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                              <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                              <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="bottom">
+                  <p>&copy; 2023 - Todos los derechos reservados</p>
+>>>>>>> Stashed changes
                 </div>
             </footer> 
         `;
