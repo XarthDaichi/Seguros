fc65093f0e6ac9336eb04d0a73af4c7ae0235cc6
LewMontes <101229716+LewMontes@users.noreply.github.com>
2023-06-04 22:37:45 -0600

    Not showing categories and coverages

--- SegurosFrontEnd/src/main/webapp/js/coverages.js
+++ SegurosFrontEnd/src/main/webapp/js/coverages.js
@@ -1,15 +1,25 @@
 class Coverages {
     dom;
+    
+    categoryModal;
+    coverageModal;
+    
     state;
     
     constructor() {
         this.state = { 'entities': new Array(), 'entity': this.emptyEntity(), 'mode': 'A' };
         this.dom = this.render();
+        this.categoryModal = new bootstrap.Modal(this.dom.querySelector("#createCategoryModal"));
+        this.coverageModal = new bootstrap.Modal(this.dom.querySelector("#createCoverageModal"));
+        
+        this.dom.querySelector("#createCategory").addEventListener('click', e=>this.categoryModal.show());
+        this.dom.querySelector("#createCoverage").addEventListener('click', e=>this.coverageModal.show());
     }
     
     render=()=> {
         const html = `
           ${this.renderList()}
+          ${this.renderModal()}
         `;
         var rootContent = document.createElement('div');
         rootContent.id = 'coverages';
@@ -23,9 +33,21 @@ class Coverages {
             <div class="row">
                 <div class="col-md-12">
                     <h1 class="text-center">Coberturas por categoría</h1>
+                    <div class="d-flex justify-content-end mb-3">
+                        <button class="btn btn-primary" id="createCategory" data-bs-toggle="modal" data-bs-target="#createCategoryModal" style="margin-right: 5px">
+                            <i class="fas fa-plus"></i> Crear categoría
+                        </button>
+                        <button class="btn btn-primary" id="createCoverage" data-bs-toggle="modal" data-bs-target="#createCoverageModal" style="margin-left: 5px">
+                            <i class="fas fa-plus"></i> Crear cobertura
+                        </button>
+                    </div>
                     <div class="card">
                         <div class="card-body">
-                            <div id="clients" class="table-responsive">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" id="searchInput" placeholder="Buscar por número categor">
+                                <button class="btn btn-outline-secondary" type="button" id="searchButton">Buscar</button>
+                            </div>
+                            <div id="categories" class="table-responsive">
                             </div>
                         </div>
                     </div>
@@ -35,6 +57,192 @@ class Coverages {
       `;
     };
     
-    emptyEntity =()=> {
+    getCategories=async()=>{
+        const categoriesRequest = new Request(`${backend}/categories`, {
+            method:'GET',
+            headers: {'Content-Type': 'application/json'}
+        });
+        
+        try{
+            const response = await fetch(categoriesRequest);
+            
+            if (!response.ok) {
+               const error = await response.text();
+               console.log("GET CATEGORIES ERROR - " + error);
+               return;
+            }
+            
+            console.log("CATEGORIES AQUIRED");
+            const categoriesList = await response.json();
+            this.state.categoriesList = categoriesList;
+            this.renderCategories();
+        }catch(err){
+            console.error(err);
+        }
+    }
+    
+    renderCategories=async()=>{
+        const tableBody = this.dom.querySelector("#categories");
+        tableBody.innerHTML = '';
+        
+        this.state.categoriesList.forEach((category)=> {
+            const row = document.createElement('div');
+            row.id = `category${category.id}`;
+            row.innerHTML = `
+                <div id="categoryIdNameDescription">
+                    <h5>${category.id} - ${category.name}</h5>
+                    <p>${category.description}</p>
+                </div>
+                <div id="categoryCoverages">
+                    <table class="table table-striped" id="categoryCoveragesTable">
+                        <thead>
+                            <tr>
+                                <th>Cobertura ID</th>
+                                <th>Nombre</th>
+                                <th>Descripción</th>
+                                <th>Costo mínimo</th>
+                                <th>Porcentaje</th>
+                            </tr>
+                        </thead>
+                        <tbody id="categoryCoveragesTableBody${category.id}">
+                        </tbody>
+                    </table>
+                </div>
+            `;
+            tableBody.appendChild(row);
+            this.renderCoverages(category);
+        });
+        
+    }
+    
+    renderCoverages=async(category)=>{
+        try{
+            const tableBody = this.dom.querySelector(`#categoryCoveragesTableBody${category.id}`);
+            tableBody.innerHTML = '';
+        
+            category.coverages.forEach((coverage) => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${coverage.id}</td>
+                    <td>${coverage.name}</td>
+                    <td>${coverage.description}</td>
+                    <td>${coverage.minimumCost}</td>
+                    <td>${coverage.percentage}</td>
+                `;
+
+                tableBody.appendChild(row);
+            });
+        }catch(err){
+            console.error(err);
+        }
+    }
+    
+    renderModal=()=>{
+        return `
+            <div id="createCategoryModal" class="modal fade" tabindex="-1">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header" >
+                            <h5 class="modal-title">Crear categoría de coberturas</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <form id="categoryForm" >
+                            <div class="modal-body">
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">Nombre</span>
+                                    <input type="text" class="form-control" id="name" name="name">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">Descripción</span>
+                                    <input type="text" class="form-control" id="description" name="description">
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button id="registerCategoryButton" type="button" class="btn btn-primary" >Aplicar</button>
+                            </div>
+                        </form>                 
+                    </div>         
+                </div>          
+            </div>
+        
+            <div id="createCoverageModal" class="modal fade" tabindex="-1">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header" >
+                            <h5 class="modal-title">Crear cobertura</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <form id="form" >
+                        <div class="modal-body">
+                            <div class="input-group mb-3">
+                                <span class="input-group-text">Nombre</span>
+                                <input type="text" class="form-control" id="name" name="name">
+                            </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text">Descripción</span>
+                                <input type="text" class="form-control" id="description" name="description">
+                            </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text">ID categoría</span>
+                                <input type="text" class="form-control" id="categoryId" name="categoryId">
+                            </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text">Costo mínimo</span>
+                                <input type="text" class="form-control" id="minimumCost" name="minimumCost">
+                            </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text">Porcentaje</span>
+                                <input type="text" class="form-control" id="percentage" name="percentage">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button id="registerCoverageButton" type="button" class="btn btn-primary" >Aplicar</button>
+                        </div>
+                        </form>                 
+                    </div>         
+                </div>          
+            </div>
+        `;     
+    }
+  
+    load=()=>{
+        // Save modal form data into entity        
+    }
+    
+    reset=()=>{
+        this.state.entity=this.emptyEntity();
+    }
+    
+    emptyEntity=()=>{
+        // return an empty entity
+    }
+
+
+    add=()=>{
+      // Validate data, load into entity, invoque backend for adding
+        this.list();
+        this.reset();
+        this.modal.hide();
+    } 
+  
+    update=()=>{
+      // Validate data, load into entity, invoque backend for updating    
+      this.list();
+      this.reset();
+      this.modal.hide();
+    }
+  
+    validate=()=>{
+       // validate data
+    }
+  
+    makenew=()=>{
+        this.reset();
+        this.state.mode='A'; //adding
+        this.showModal();
+    }
+    
+    search=async()=>{
+        
     }
 }
